cff35c3a802d6ecb484c2d49854d9a7ec3a1838e
Owl-Code <Paul.einwich@gmail.com>
2018-03-28 18:16:16 -0700

    Ui Test Branch
    New UI

--- box_o_vox_client.lua
+++ box_o_vox_client.lua
@@ -2,15 +2,15 @@ box_o_vox = {
    constants = require 'constants'
 }
 local log = radiant.log.create_logger('log')
-log:error("Box o' Vox Server: Alpha 23 mod")
+log:error("Box o' Vox client: Alpha 23 mod")
 
-function box_o_vox:_on_required_loaded()
-  --[[  local CustomPortraitRendererService = require('services.client.portrait_renderer.portrait_renderer_service')
+function box_o_vox:_on_init()
+    local CustomPortraitRendererService = require('services.client.portrait_renderer.portrait_renderer_service')
     local PortraitRendererService = radiant.mods.require('stonehearth.services.client.portrait_renderer.portrait_renderer_service')
     radiant.mixin(PortraitRendererService, CustomPortraitRendererService)
-    --]]
+    
 end
 
-radiant.events.listen_once(radiant,'radiant:required_loaded', box_o_vox, box_o_vox._on_required_loaded)
+radiant.events.listen_once(radiant,'radiant:client:server_ready', box_o_vox, box_o_vox._on_init)
 
 return box_o_vox

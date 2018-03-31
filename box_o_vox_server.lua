bc13faada3ac28ad66a30ecb2f2d134d8312a900
Owl-Code <Paul.einwich@gmail.com>
2018-03-30 17:22:32 -0700

    Camp Generation
    - Allowed camp citizen location placement to be dynamic
    - fixed double ai in necromancer abilities

--- box_o_vox_server.lua
+++ box_o_vox_server.lua
@@ -5,10 +5,9 @@ local log = radiant.log.create_logger('log')
 log:error("Box o' Vox Server: Alpha 23 mod")
 
 function box_o_vox:_on_required_loaded()
-  --[[  local CustomPortraitRendererService = require('services.client.portrait_renderer.portrait_renderer_service')
-    local PortraitRendererService = radiant.mods.require('stonehearth.services.client.portrait_renderer.portrait_renderer_service')
-    radiant.mixin(PortraitRendererService, CustomPortraitRendererService)
-    --]]
+  local CustomGameCreationService = require('services.server.game_creation.game_creation_service')
+    local GameCreationService = radiant.mods.require('stonehearth.services.server.game_creation.game_creation_service')
+    radiant.mixin(GameCreationService, CustomGameCreationService)
 end
 
 radiant.events.listen_once(radiant,'radiant:required_loaded', box_o_vox, box_o_vox._on_required_loaded)

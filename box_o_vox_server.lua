4d6e79574f0e6bd6938fcd487174544f4939ecde
Owl-Code <Paul.einwich@gmail.com>
2018-04-14 22:22:17 -0700

    I just stashed this hope it works fingers crossed &

--- box_o_vox_server.lua
+++ box_o_vox_server.lua
@@ -9,11 +9,11 @@ function box_o_vox:_on_required_loaded()
     local GameCreationService = radiant.mods.require('stonehearth.services.server.game_creation.game_creation_service')
     radiant.mixin(GameCreationService, CustomGameCreationService)
     
-    local config = radiant.util.get_config('boxland')
+    local config = radiant.util.get_config('rivers.boxland')
 	if not config then
-		radiant.util.set_config("boxland.narrow_river_counter", 5)
-		radiant.util.set_config("boxland.wide_river_counter", 3)
-		radiant.util.set_config("boxland.keep_original_map_lakes", false)
+		radiant.util.set_config("rivers.boxland.narrow_river_counter", 5)
+		radiant.util.set_config("rivers.boxland.wide_river_counter", 3)
+		radiant.util.set_config("rivers.boxland.keep_original_map_lakes", false)
 	end
 end
 

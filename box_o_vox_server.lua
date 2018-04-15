c1ea08ee6000165ba595765967887b986bef2e7e
Owl-Code <Paul.einwich@gmail.com>
2018-04-14 23:43:43 -0700

    changed rivers to look very nice thanks @brunosupremo

--- box_o_vox_server.lua
+++ box_o_vox_server.lua
@@ -2,19 +2,12 @@ box_o_vox = {
    constants = require 'constants'
 }
 local log = radiant.log.create_logger('log')
-log:error("Box o' Vox Server: Alpha 23 mod")
+log:error("Box o' Vox Server: Alpha 24 mod")
 
 function box_o_vox:_on_required_loaded()
     local CustomGameCreationService = require('services.server.game_creation.game_creation_service')
     local GameCreationService = radiant.mods.require('stonehearth.services.server.game_creation.game_creation_service')
     radiant.mixin(GameCreationService, CustomGameCreationService)
-    
-    local config = radiant.util.get_config('rivers.boxland')
-	if not config then
-		radiant.util.set_config("rivers.boxland.narrow_river_counter", 5)
-		radiant.util.set_config("rivers.boxland.wide_river_counter", 3)
-		radiant.util.set_config("rivers.boxland.keep_original_map_lakes", false)
-	end
 end
 
 radiant.events.listen_once(radiant,'radiant:required_loaded', box_o_vox, box_o_vox._on_required_loaded)

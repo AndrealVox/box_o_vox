435ce6f3dd1ad786b544a98d96e351fc628ac48a
Owl-Code <Paul.einwich@gmail.com>
2018-05-29 14:23:10 -0700

    Beehive trapfields for all

--- box_o_vox_server.lua
+++ box_o_vox_server.lua
@@ -4,7 +4,10 @@ box_o_vox = {
 local log = radiant.log.create_logger('log')
 log:error("Box o' Vox Server: Alpha 24 mod")
 
-function box_o_vox:_on_required_loaded()
+function box_o_vox:_on_required_loaded()    
+    local BeeTrapper = require('jobs.trapper')
+    local BeeTrappingGrounds = require('components.trapping.trapping_grounds_component')
+
     local CustomGameCreationService = require('services.server.game_creation.game_creation_service')
     local GameCreationService = radiant.mods.require('stonehearth.services.server.game_creation.game_creation_service')
     radiant.mixin(GameCreationService, CustomGameCreationService)
@@ -12,11 +15,6 @@ function box_o_vox:_on_required_loaded()
     local CustomHeightMapRenderer = require('services.server.world_generation.custom_height_map_renderer')
     local HeightMapRenderer = radiant.mods.require('stonehearth.services.server.world_generation.height_map_renderer')
     radiant.mixin(HeightMapRenderer, CustomHeightMapRenderer)
-
-    
-    local BeeTrapper = require('jobs.trapper')
-    local BeeTrappingGrounds = require('components.trapping.trapping_grounds_component')
-
 end
 
 radiant.events.listen_once(radiant,'radiant:required_loaded', box_o_vox, box_o_vox._on_required_loaded)

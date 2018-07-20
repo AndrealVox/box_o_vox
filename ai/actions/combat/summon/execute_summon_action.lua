de907d1003c5a64f5c8fd1c3b40f761408b9b02d
AndrealVox <andrealvox@gmail.com>
2018-07-20 08:17:06 -0700

    Summon Despawn
    -Added a line of code from Max

--- ai/actions/combat/summon/execute_summon_action.lua
+++ ai/actions/combat/summon/execute_summon_action.lua
@@ -167,6 +167,12 @@ function ExecuteSummon:_create_summon(summoner, target, summon_uri)
     for _, summon in pairs(summons) do
         radiant.entities.set_player_id(summon, 'player_1') 
         radiant.entities.add_buff(summon,'box_o_vox:data:buffs:summon_timer')
+    local SUMMON_TIME = '1h'
+    stonehearth.calendar:set_timer('despawn summon', SUMMON_TIME, function()
+         if radiant.entities.exists(summon) then
+            radiant.entities.destroy_entity(summon)
+         end
+      end)
         --radiant.effects.run_effect(summon, 'stonehearth:effects:firepit_effect:green')
     end
     return summons

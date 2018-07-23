0aeb8705fdde96f72af7f9510fba0cc2559cfc74
AndrealVox <andrealvox@gmail.com>
2018-07-23 08:37:19 -0700

    Major Balance Update
    -Updated WAAAAAAAAY too many things to post them all. Just know almost ALL items are scaled to the default values of crops, networth and hunger among other balancing.

--- box_o_vox_server.lua
+++ box_o_vox_server.lua
@@ -1,10 +1,10 @@
 box_o_vox = {
    constants = require 'constants'
 }
-local log = radiant.log.create_logger('log')
-log:error("Box o' Vox Server: Alpha 24 mod")
-
-function box_o_vox:_on_required_loaded()    
+function box_o_vox:_on_biome_set(e)
+	if e.biome_uri ~= "box_o_vox:biome:boxland" then
+		return
+	end
     local CustomGameCreationService = require('services.server.game_creation.game_creation_service')
     local GameCreationService = radiant.mods.require('stonehearth.services.server.game_creation.game_creation_service')
     radiant.mixin(GameCreationService, CustomGameCreationService)
@@ -14,6 +14,6 @@ function box_o_vox:_on_required_loaded()
     radiant.mixin(HeightMapRenderer, CustomHeightMapRenderer)
 end
 
-radiant.events.listen_once(radiant,'radiant:required_loaded', box_o_vox, box_o_vox._on_required_loaded)
+radiant.events.listen_once(radiant, 'stonehearth:biome_set', box_o_vox, box_o_vox._on_biome_set)
 
 return box_o_vox

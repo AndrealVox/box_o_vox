7ced812de439f1eaf33bb8fb60eaf38df0794fba
AndrealVox <andrealvox@gmail.com>
2018-08-20 17:00:49 -0700

    Height Map
    -Bruno rewrote grass on mountain lua for us :smiley: what a great guy
    -PNGs to go with update

--- services/server/world_generation/custom_height_map_renderer.lua
+++ services/server/world_generation/custom_height_map_renderer.lua
@@ -1,14 +1,6 @@
-local Array2D = require 'services.server.world_generation.array_2D'
-local Rect2 = _radiant.csg.Rect2
-local Point2 = _radiant.csg.Point2
 local Cube3 = _radiant.csg.Cube3
 local Point3 = _radiant.csg.Point3
-local Region2 = _radiant.csg.Region2
-local Region3 = _radiant.csg.Region3
-local HeightMapCPP = _radiant.csg.HeightMap
 
-local log = radiant.log.create_logger('CHMR')
-log:always("Created CHMR")
 local CustomHeightMapRenderer = class()
 
 function CustomHeightMapRenderer:_add_mountains_to_region(region3, rect, height)
@@ -19,8 +11,9 @@ function CustomHeightMapRenderer:_add_mountains_to_region(region3, rect, height)
 
 	block_min = 0
 
-	--plains = 10, foothills = 15, mountains = 95
-	local min_grass_height = 2 --last 2 steps
+	local min_grass_height = 0
+	local max_grass_height = 120
+
 	for i=1, num_rock_layers do
 		if (i == num_rock_layers) or (height <= rock_layers[i].max_height) then
 			block_max = height
@@ -29,7 +22,7 @@ function CustomHeightMapRenderer:_add_mountains_to_region(region3, rect, height)
 			block_max = rock_layers[i].max_height
 		end
 
-		local has_grass = stop and block_max > min_grass_height
+		local has_grass = stop and block_max >= min_grass_height and block_max <= max_grass_height
 		local rock_top = has_grass and block_max-1 or block_max
 
 		region3:add_unique_cube(Cube3(
@@ -39,7 +32,7 @@ function CustomHeightMapRenderer:_add_mountains_to_region(region3, rect, height)
 		))
 		
 		if has_grass then 
-			local material = self._block_types.grass_hills or self._block_types.grass
+			local material = self._block_types.grass_hills
 			region3:add_unique_cube(Cube3(
 				Point3(rect.min.x, rock_top, rect.min.y),
 				Point3(rect.max.x, block_max, rect.max.y),
